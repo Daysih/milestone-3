6dab3020b844a609ae36b79ad64fea58593f492a
daysi <dvh7@humboldt.edu>
2020-02-21 14:47:35 -0800

    some progress

---
+++ milestonethree/Assets/Scripts/card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public GameObject card;
+       string name { get; set; }
+
+    public card()
+    {
+        name = " ";
+
+    }
+
+        
+}
+void flip()
+{
+
+
+}
